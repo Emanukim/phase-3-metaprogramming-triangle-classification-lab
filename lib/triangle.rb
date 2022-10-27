504bc53cdd46f230d83a3298c6617db6ca98a813
Emanukim <emmanue.kimweli@student.moringaschool.com>
2022-10-27 16:00:05 +0300

    Completed lab

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,60 @@
 class Triangle
-  # write code here
+  attr_accessor :line_x, :line_y, :line_z
+
+  def initialize line_x,line_y,line_z
+    @line_x = line_x
+    @line_y = line_y
+    @line_z = line_z
+  end 
+
+  def kind
+    if  line_x <= 0 || line_y <= 0 || line_z <= 0
+      raise TriangleError
+    elsif (line_x + line_y) <= line_z || (line_x + line_z) <= line_y || (line_y + line_z) <= line_x
+      raise TriangleError
+    elsif line_x == line_y && line_x == line_z
+      :equilateral
+    elsif line_x == line_y || line_x == line_z || line_y == line_z
+      :isosceles
+    else line_x != line_y || line_x != line_z || line_y != line_z
+      :scalene
+    end
+
+
+  end
+
+
+
+     class TriangleError < StandardError
+     end
+ 
+
+
+
+
 end
+
+
+ # write code here
+
+  # attr_accessor :
+
+  # def initialize()
+  # end
+
+  # def 
+  #   if
+  #   else
+  #     begin
+  #       raise TriangleError
+  #     rescue Triangle => error
+  #       puts error.message
+  #     end
+  #   end
+  # end
+
+  # class TriangleError < StandardError
+  #   def message
+  #     ""
+  #   end
+  # end
